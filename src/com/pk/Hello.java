6b1dc27db1d1cd1c5df8601c24a516ce3eaf7ce5
mahesh.karad <mahesh.karad@apisero.com>
2020-11-23 09:58:20 +0530

    version 1

--- src/com/pk/Hello.java
+++ src/com/pk/Hello.java
@@ -4,7 +4,8 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.println("hello welcome");
+        System.out.println("hello my name is mahesh karad");
+	  
 	}
 
 }
