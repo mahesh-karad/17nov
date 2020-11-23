6bc1f5cc6efda1ab97f1a2ae44356314bcef1732
DELL <DELL@Vivek>
2020-11-23 10:38:57 +0530

    reset

--- src/com/pk/Hello.java
+++ src/com/pk/Hello.java
@@ -5,7 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("hello my name is mahesh karad");
-	  
+	    System.out.println("Hello from Vivek Doshi");
 	}
 
 }
