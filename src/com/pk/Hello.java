d59fe6a2baef347117665f76fc6f1b6eed87fae6
dell <DELL@DESKTOP-T9INLHE>
2020-11-23 12:51:36 +0530

    commit by mousam-version1

--- src/com/pk/Hello.java
+++ src/com/pk/Hello.java
@@ -10,6 +10,8 @@ public class Hello {
 	    System.out.println("Hello from Ramakant!");
 	    System.out.println("Hello from Vaishnavi!!");
 	    System.out.println("Hello from Mousam");
+	    mousam m = new mousam("Mousam",101,"Bangalore");
+	    System.out.println(m);
 	}
 
 }
