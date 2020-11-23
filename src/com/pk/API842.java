d568b6a41d47e3937eff94e09335a7d56d8f70db
vaishnavi2302 <vaishnavitiwari68@gmail.com>
2020-11-23 12:52:47 +0530

    Added a new file, API842.java

---
+++ src/com/pk/API842.java
@@ -0,0 +1,28 @@
+package com.pk;
+
+public class API842 {
+	
+	private String empId;
+	private String name;
+	private String city;
+	
+		
+	public API842(String empId, String name, String city) {
+		super();
+		this.empId = empId;
+		this.name = name;
+		this.city = city;
+	}
+
+	@Override
+	public String toString() {
+		return "Details:  [empId=" + empId + ", name=" + name + ", city=" + city + "]";
+	}
+
+	public static void main(String[] args) {
+		
+		API842 obj = new API842("API842", "Vaishnavi","Dehradun");
+		System.out.println(obj.toString());	
+	}
+
+}
