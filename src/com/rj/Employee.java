6dc9928a0e4ceeb77709ce680c6b0596d0bc2491
Rakshit Jain <rakshit.jain@apisero.com>
2020-11-23 13:33:06 +0530

    Version 2 RJ

---
+++ src/com/rj/Employee.java
@@ -0,0 +1,18 @@
+package com.rj;
+
+public class Employee {
+	private int id;
+	private String name;
+	private float salary;
+	public Employee(int id, String name, float salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", salary=" + salary + "]";
+	}
+	
+}
---
+++ src/com/rj/Employer.java
@@ -0,0 +1,11 @@
+package com.rj;
+
+public class Employer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Employee e1 = new Employee(101,"RJ", 243910.50f);
+		System.out.println(e1.toString());
+	}
+
+}
